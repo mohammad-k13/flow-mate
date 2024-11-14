7e52f5f0a770950eac51231b9ca2b55ca27e1b7e
mohammad-k13 <mohammadkeshsavarz.a.13@gmail.com>
2024-11-14 23:14:18 +0330

    TASK - Add SignOut button to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,19 @@
+import { signOut } from '@/auth'
+import Slack from '@/components/layout/slack'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  const onClick =  async () => {
+    "use server"
+    await signOut();
+  }
   return (
-    <div>Dashboard</div>
+    <section className='w-full h-screen'>
+      <Slack className='w-full h-full'>
+        <Button onClick={onClick}>Sign Out</Button>
+      </Slack>
+    </section>
   )
 }
 
